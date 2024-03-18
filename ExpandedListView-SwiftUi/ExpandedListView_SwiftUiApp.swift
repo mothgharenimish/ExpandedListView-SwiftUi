c14eaed095b4ef6d98fc78f1827cb4355d1dd17d
Nimish900 <nimishmothghare0@gmail.com>
2024-03-18 17:23:40 +0530

    Initial Commit

---
+++ ExpandedListView-SwiftUi/ExpandedListView_SwiftUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpandedListView_SwiftUiApp.swift
+//  ExpandedListView-SwiftUi
+//
+//  Created by Nimish Mothgare on 18/03/24.
+//
+
+import SwiftUI
+
+@main
+struct ExpandedListView_SwiftUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
